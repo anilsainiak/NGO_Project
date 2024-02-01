6a66dc14dbdebd83f4642498b5f0b8746fcf2ffb
anil saini <anilsaini1012@gmail.com>
2024-02-01 20:56:59 +0530

    new changes

--- routes/data.js
+++ routes/data.js
@@ -90,7 +90,7 @@ router.post("/add",async (req,res)=>{
 //GET ALL
 router.get("/view",async (req,res)=>{
     try{
-        const data=await Data.find();
+        const data=await Data.find().sort({receiptNumber:1});
         res.status(200).json(data);
     }catch(err){
         res.status(500).json(err);
@@ -106,7 +106,5 @@ router.get('/view/:id',async (req,res)=>{
         res.status(500).json(err);
     }
 })
-
-
 
 module.exports=router;
